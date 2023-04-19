3be6ad63cb2dc35ba8b493ade5d4755451063704
Matthew Rowley <mattrowley10@gmail.com>
2023-04-19 13:29:56 -0600

    Home page w/ fetch

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { getPosts } from "../api/helpers";
 
 export default function AllPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getAllPosts() {
+      try {
+        const allPosts = await getPosts();
+        setPosts(allPosts);
+      } catch (error) {
+        console.error("Oops");
+      }
+    }
+    getAllPosts();
+  }, []);
+
   return (
     <div className="allposts">
-      <div className="one-post">
-        <h1 className="post-title">Post Title</h1>
-        <p className="post-body">
-          This is the stuff that will be written in the post body This is the
-          stuff that will be written in the post body This is the stuff that
-          will be written in the post body This is the stuff that will be
-          written in the post body This is the stuff that will be written in the
-          post body the post body This is the stuff that will be written in the
-          post body the post body This is the stuff that will be written in the
-          post body
-        </p>
-        <button className="details-button">Details</button>
-      </div>
+      {posts.map((post) => {
+        return (
+          <div className="one-post" key={post._id}>
+            <h1 className="post-title">{post.title}</h1>
+            <p className="post-body">{post.description}</p>
+            <p className="author">User: {post.author.username}</p>
+            <button className="details-button">Details</button>
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 }
--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,6 +1,12 @@
 const cohortName = '2301-FTB-ET-WEB-AM'
 const baseUrl = 'https://strangers-things.herokuapp.com/api/${cohortName}'
 
-export const getAllPosts = async () => {
-    
-}
+export const getPosts = async () => {
+	try {
+		const response = await fetch(`${baseUrl}/posts`);
+		const result = await response.json();
+		return result.data.posts;
+	} catch (error) {
+		console.error("ERROR");
+	}
+};
